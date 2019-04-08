a3ed07ae7f38c458858006b0194a575e0cdf6607
DANIV18 <igordaniv242@gmail.com>
2019-04-08 22:00:58 +0300

    was modified creature class

--- SimpleRPG/creature.cpp
+++ SimpleRPG/creature.cpp
@@ -1,6 +1,41 @@
 #include "creature.h"
 
-Creature::Creature()
+char Creature::symbol() const
 {
+    return mSymbol;
+}
 
+int Creature::health() const
+{
+    return mHealth;
+}
+
+int Creature::damage() const
+{
+    return mDamage;
+}
+
+int Creature::gold() const
+{
+    return mGold;
+}
+
+void Creature::reduceHealth(int health)
+{
+    mHealth -= health;
+}
+
+bool Creature::isDead()
+{
+    return mHealth <= 0;
+}
+
+void Creature::addGold(int gold)
+{
+    mGold += gold;
+}
+
+std::string Creature::name() const
+{
+    return mName;
 }
--- SimpleRPG/creature.h
+++ SimpleRPG/creature.h
@@ -1,11 +1,27 @@
 #ifndef CREATURE_H
 #define CREATURE_H
 
+#include <string>
 
 class Creature
 {
+private:
+    std::string mName;
+    char mSymbol;
+    int mHealth;
+    int mDamage;
+    int mGold;
+
 public:
-    Creature();
+    void reduceHealth(int health);
+    bool isDead();
+    void addGold(int gold);
+
+    std::string name() const;
+    char symbol() const;
+    int health() const;
+    int damage() const;
+    int gold() const;
 };
 
 #endif // CREATURE_H
